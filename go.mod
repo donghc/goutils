82d4a7dd7406e45777845222fa437690c3341ae8
donghongchen <donghongchen@threatbook.cn>
2022-02-22 10:05:37 +0800

    企业微信报警

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/donghc/goutils
 
 go 1.17
 
-require github.com/coreos/etcd v3.3.10+incompatible
+require (
+	github.com/coreos/etcd v3.3.10+incompatible
+	github.com/sirupsen/logrus v1.8.1
+	go.uber.org/zap v1.19.1
+)
 
 require (
 	github.com/coreos/bbolt v1.3.4 // indirect
@@ -19,12 +23,14 @@ require (
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.11.0 // indirect
 	github.com/soheilhy/cmux v0.1.5 // indirect
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20220101234140-673ab2c3ae75 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
-	go.uber.org/zap v1.19.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
@@ -33,6 +39,7 @@ require (
 	google.golang.org/genproto v0.0.0-20200513103714-09dca8ec2884 // indirect
 	google.golang.org/grpc v1.43.0 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
 
 replace github.com/coreos/bbolt v1.3.4 => go.etcd.io/bbolt v1.3.4
---
+++ pkg/alerthook/workwx.go
@@ -0,0 +1,135 @@
+package alerthook
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"go.uber.org/zap/zapcore"
+)
+
+// https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=a544c793-4800-4e12-b739-66fc47250dfa
+
+type workWxHook struct {
+	client    *http.Client
+	subject   string
+	key       string
+	apiURL    string
+	mentioned []string
+}
+
+// https://work.weixin.qq.com/api/doc/90000/90136/91770
+type workWxMsgContent struct {
+	Content       string   `json:"content"`
+	MentionedList []string `json:"mentioned_list"`
+	MobileList    []string `json:"mentioned_mobile_list"`
+}
+
+type workWxMsg struct {
+	MsgType string           `json:"msgtype"` // text/markdown/image/news
+	Text    workWxMsgContent `json:"text"`
+}
+
+func NewWorkWxHook(client *http.Client, subject string, key string, mentioned []string) *workWxHook {
+	if subject == "" {
+		subject = os.Args[0]
+	}
+	if client == nil {
+		client = &http.Client{}
+	}
+
+	return &workWxHook{
+		subject:   subject,
+		key:       key,
+		client:    &http.Client{},
+		apiURL:    "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + key,
+		mentioned: mentioned,
+	}
+}
+
+func (self *workWxHook) Levels() []logrus.Level {
+	return []logrus.Level{logrus.ErrorLevel, logrus.PanicLevel}
+}
+
+func (self *workWxHook) Fire(entry *logrus.Entry) error {
+	return self.sendLogrus(entry)
+}
+
+func (self *workWxHook) sendLogrus(entry *logrus.Entry) error {
+	if self.key == "" {
+		return nil
+	}
+	client := self.client
+
+	var msg workWxMsg
+	msg.MsgType = "text"
+	txt := &msg.Text
+	txt.Content = self.subject + "\n" + entry.Message
+	if len(self.mentioned) > 0 {
+		txt.MobileList = self.mentioned
+	} else {
+		txt.MentionedList = []string{"@all"}
+	}
+	body, _ := json.Marshal(msg)
+
+	req, err := http.NewRequest("POST",
+		self.apiURL,
+		bytes.NewBuffer(body))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("post workWx:", err)
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func (self *workWxHook) sendZap(entry zapcore.Entry) error {
+	if self.key == "" {
+		return nil
+	}
+	var flag bool
+	for _, level := range self.Levels() {
+		if level.String() == entry.Level.String() {
+			flag = true
+			break
+		}
+	}
+	if !flag {
+		return nil
+	}
+	client := self.client
+
+	var msg workWxMsg
+	msg.MsgType = "text"
+	txt := &msg.Text
+	txt.Content = entry.Message
+	if len(self.mentioned) == 0 {
+		txt.MentionedList = []string{"@all"}
+	} else {
+		txt.MobileList = self.mentioned
+	}
+	body, _ := json.Marshal(msg)
+
+	req, err := http.NewRequest("POST",
+		self.apiURL,
+		bytes.NewBuffer(body))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("post sms:", err)
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func (self *workWxHook) LogrusHook() logrus.Hook {
+	return self
+}
+
+func (self *workWxHook) ZapHook() func(zapcore.Entry) error {
+	return self.sendZap
+}

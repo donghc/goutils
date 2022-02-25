b22253b4588f4520804310e5dbcf3f262efcedbc
donghongchen <donghongchen@threatbook.cn>
2022-02-25 16:45:41 +0800

    配置文件公共方法

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,44 +1,98 @@
 package config
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/donghc/goutils/pkg/file"
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-// Load returns an application configuration which is populated
-// from the given configuration file.
-func Load(path, env string, c interface{}) error {
+func Load(path string, c interface{}) {
+	dir := filepath.Dir(path)
+	filename := filepath.Base(path)
+	ext := filepath.Ext(path)
 
-	// Adding our TOML config file.
-	viper.AddConfigPath(path)
-
-	// Load the config type depending on env variable.
-	var name string
-	switch env {
-	case "local":
-		name = "local"
-	case "dev":
-		name = "dev"
-	case "prod":
-		name = "prod"
-	default:
-		name = "local"
+	viper.SetConfigType(strings.ReplaceAll(ext, ".", ""))
+	//不包含扩展名
+	viper.SetConfigName(filename)
+	//路径
+	viper.AddConfigPath(dir)
+	_, ok := file.IsExists(path)
+	if !ok {
+		panic(errors.New(fmt.Sprintf("file not fount : %v", path)))
 	}
 
-	// Set the config name to choose from the config path
-	// Extension not needed.
-	viper.SetConfigName(name)
-
-	// Load the configuration from disk.
-	err := viper.ReadInConfig()
-	if err != nil {
-		return err
+	bys, err := ioutil.ReadFile(path)
+	if os.IsNotExist(err) {
+		panic(err)
 	}
 
-	// Unmarshals the config into our interface.
-	err = viper.Unmarshal(&c)
-	if err != nil {
-		return err
+	var r io.Reader = bytes.NewReader(bys)
+	if err := viper.ReadConfig(r); err != nil {
+		panic(err)
+	}
+	if err := viper.Unmarshal(&c); err != nil {
+		panic(err)
 	}
 
-	return err
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		if err := viper.Unmarshal(&c); err != nil {
+			panic(err)
+		}
+	})
+}
+
+func Load1(path string, c interface{}) {
+	dir := filepath.Dir(path)
+	filename := filepath.Base(path)
+	ext := filepath.Ext(path)
+
+	viper.SetConfigType(strings.ReplaceAll(ext, ".", ""))
+	//不包含扩展名
+	viper.SetConfigName(filename)
+	//路径
+	viper.AddConfigPath(dir)
+	_, ok := file.IsExists(path)
+	if !ok {
+		if err := os.MkdirAll(filepath.Dir(path), 0766); err != nil {
+			panic(err)
+		}
+		f, err := os.Create(path)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		if err := viper.WriteConfig(); err != nil {
+			panic(err)
+		}
+	}
+
+	bys, err := ioutil.ReadFile(path)
+	if os.IsNotExist(err) {
+		panic(err)
+	}
+
+	var r io.Reader = bytes.NewReader(bys)
+	if err := viper.ReadConfig(r); err != nil {
+		panic(err)
+	}
+	if err := viper.Unmarshal(&c); err != nil {
+		panic(err)
+	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		if err := viper.Unmarshal(&c); err != nil {
+			panic(err)
+		}
+	})
 }

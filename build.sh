01c5edd37837d1139bc78fa958c0864776e9c341
donghongchen <donghongchen@threatbook.cn>
2022-03-09 11:09:38 +0800

    更新脚本

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+app=$1
+apps=( "csscand" "csmeta" "csfront" "csinterface" "cspost" "csagg" "cswatch" "csadmin" "metric")
 
 function prebuild() {
 #    FULL_Version=`git tag -l "$APPNAME-*"|tail -n 1`
     Version=`git describe $(git rev-list --tags --max-count=1)`
-    Author=`git show ${Version} | grep 'Tagger' |  cut -d ' ' -f2`
+    #Author=`git show ${Version} | grep 'Tagger' |  cut -d ' ' -f2`
+    Author=`git config --get user.name`
     BuildDate=$(date "+%Y-%m-%dT%H:%M:%S")
     # git describe $(git rev-list --tags --max-count=1)
 #    FULL_Version=`git  branch | grep '*' | sed -e 's/\*//g' -e 's/HEAD detached at//g' -e 's/\s*//g' -e 's/[\(\)]//g'`
@@ -15,12 +18,15 @@ function prebuild() {
 #    REVISION=`git rev-parse --short HEAD`
 #    echo "prepare build:  $Version#$APPNAME"
 #    REVISION=`git rev-parse --short HEAD`
+#    GCFLAGS="-N -l"
+    #GCFLAGS="'-s -w'"
     #LDFLAGS="-X main.APPVersion=${Version} -X main.REVISION=${REVISION} -X main.APPNAME=${APPNAME}"
     #LDFLAGS="-s -w -X main.APPVersion=${Version}  -X main.REVISION=${REVISION} -X main.APPNAME=${APPNAME} -extldflags '-static'"
     LDFLAGS=" -X main.Version=${Version}  -X main.Author=${Author} -X main.BuildDate=${BuildDate}"
 
     ##generate resource first
 }
+
 prebuild
 echo "Version="${Version}
 echo "Author="${Author}
@@ -28,13 +34,27 @@ echo "BuildDate="${BuildDate}
 echo "LDFLAGS="${LDFLAGS}
 echo "build start"
 rm -rf build/*
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/csadmin $PWD/main/csadmin/.
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/csagg $PWD/main/csagg/.
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/csfront $PWD/main/csfront/.
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/csinterface $PWD/main/csinterface/.
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/csmeta $PWD/main/csmeta/.
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/cspost $PWD/main/cspost/.
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/csscand $PWD/main/csscand/.
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/cswatch $PWD/main/cswatch/.
-#go build --ldflags "$LDFLAGS" -o $PWD/build/csscand.exe $PWD/main/csscand/.
+
+app=$1
+
+function build() {
+app=$1
+echo "building $app..."
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "$LDFLAGS" -o "$PWD/build/$app" "$PWD/main/$app/."
+if [ "$app" == "csscand" ]; then
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build --ldflags "$LDFLAGS" -o $PWD/build/csscand.exe $PWD/main/csscand/.
+fi
+}
+
+if [ "x$app" == "x" ]; then
+  for app in ${apps[@]}
+  do
+     build $app
+  done
+elif [[ "${apps[@]}" =~ "${app}" ]]; then
+  build $app;
+elif [[ ! "${apps[@]}" =~ "${app}" ]]; then
+  echo "plz input right app: ${apps[@]}"
+fi
+
 echo "build end"

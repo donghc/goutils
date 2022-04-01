a57712a93c486c84744ee99c003ddd4081ca2887
donghongchen <donghongchen@threatbook.cn>
2022-04-01 17:27:56 +0800

    add test

--- go.mod
+++ go.mod
@@ -7,20 +7,21 @@ require (
 	github.com/cheggaaa/pb/v3 v3.0.8
 	github.com/coreos/etcd v3.3.10+incompatible
 	github.com/fsnotify/fsnotify v1.5.1
-	github.com/go-redis/redis/v8 v8.11.4
 	github.com/hashicorp/go-retryablehttp v0.7.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.10.1
 	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
+	github.com/uber-go/tally v3.3.15+incompatible
 	github.com/wjiec/gdsn v0.0.0-20200627112200-d508c613657a
 	github.com/xdg-go/scram v1.1.0
+	go.uber.org/cadence v0.19.0
+	go.uber.org/yarpc v1.55.0
 	go.uber.org/zap v1.19.1
 )
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/VividCortex/ewma v1.1.1 // indirect
-	github.com/anmitsu/go-shlex v0.0.0-20161002113705-648efa622239 // indirect
 	github.com/apache/thrift v0.0.0-20161221203622-b2a4d4ae21c7 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
@@ -30,15 +31,13 @@ require (
 	github.com/cristalhq/jwt/v3 v3.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/eapache/go-resiliency v1.2.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
 	github.com/fatih/color v1.13.0 // indirect
-	github.com/fatih/structtag v1.2.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/mock v1.4.4 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/btree v1.0.1 // indirect
@@ -52,9 +51,7 @@ require (
 	github.com/jcmturner/gofork v1.0.0 // indirect
 	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
-	github.com/jessevdk/go-flags v1.4.0 // indirect
 	github.com/jonboulle/clockwork v0.2.2 // indirect
-	github.com/kisielk/errcheck v1.5.0 // indirect
 	github.com/klauspost/compress v1.14.2 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
@@ -66,7 +63,6 @@ require (
 	github.com/pborman/uuid v0.0.0-20160209185913-a97ce2ca70fa // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.11.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
@@ -84,21 +80,19 @@ require (
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20220101234140-673ab2c3ae75 // indirect
-	github.com/uber-go/tally v3.3.15+incompatible // indirect
+	github.com/uber-go/mapdecode v1.0.0 // indirect
 	github.com/uber/tchannel-go v1.16.0 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/cadence v0.19.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/net/metrics v1.3.0 // indirect
 	go.uber.org/thriftrw v1.25.0 // indirect
-	go.uber.org/yarpc v1.55.0 // indirect
 	golang.org/x/crypto v0.0.0-20220128200615-198e4374d7ed // indirect
-	golang.org/x/lint v0.0.0-20200130185559-910be7a94367 // indirect
-	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/mod v0.5.0 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
@@ -108,11 +102,10 @@ require (
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
 	google.golang.org/grpc v1.43.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	honnef.co/go/tools v0.0.1-2019.2.3 // indirect
+	honnef.co/go/tools v0.0.1-2020.1.4 // indirect
 )
 
 replace github.com/coreos/bbolt v1.3.4 => go.etcd.io/bbolt v1.3.4
--- main.go
+++ main.go
@@ -1,130 +1,111 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"crypto/sha256"
-	"encoding/hex"
 	"encoding/json"
+	"flag"
 	"fmt"
-	"github.com/coreos/etcd/clientv3"
-	"log"
-	"os"
+	"net/http"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
+// https://work.weixin.qq.com/api/doc/90000/90136/91770
+type workWxMsgContent struct {
+	Content       string   `json:"content"`
+	MentionedList []string `json:"mentioned_list"`
+	MobileList    []string `json:"mentioned_mobile_list"`
+	IP            string   `json:"ip"`
+	HostName      string   `json:"hostname"`
+}
+
+type workWxMsg struct {
+	MsgType string           `json:"msgtype"` // text/markdown/image/news
+	Text    workWxMsgContent `json:"text"`
+}
+
+func wxNotify(subject, text string) {
+	var msg workWxMsg
+	msg.MsgType = "text"
+	txt := &msg.Text
+
+	txt.Content = subject + "\n" + text
+	if len(mentioned) > 0 {
+		txt.MobileList = mentioned
+	} else {
+		txt.MentionedList = []string{"@all"}
+	}
+	body, _ := json.Marshal(msg)
+	req, err := http.NewRequest("POST",
+		wxURI,
+		bytes.NewBuffer(body))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("post workWx:", err)
+	}
+	defer resp.Body.Close()
+}
+
 var (
-	config  clientv3.Config
-	err     error
-	client  *clientv3.Client
-	kv      clientv3.KV
-	putResp *clientv3.PutResponse
-	path    = "/config/engines/v1"
+	ctx       = context.Background()
+	client    = &http.Client{}
+	wxURI     = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=3e689b2c-fa1f-4f85-9462-1674dab4a0cf`
+	mentioned []string
+	max       int64
+	count     int64 = 0
+	notice          = 1
 )
 
-func t2() {
-	tm1 := "2017-03-18 17:39:59 +0000 UTC"                           //外部传入的时间字符串
-	timeTemplate1 := "2006-01-02 15:04:05 +0000 UTC"                 //常规类型
-	stamp, _ := time.ParseInLocation(timeTemplate1, tm1, time.Local) //使用parseInLocation将字符串格式化返回本地时区时间
-	log.Println(stamp.Unix())                                        //输出：1546926630
-	//获取时间戳
-
-	//timestamp := time.Now().Unix()
-	//fmt.Println(timestamp)
-	////格式化为字符串,tm为Time类型
-	//tm := time.Unix(timestamp, 0)
-	//fmt.Println(tm.Format("2006-01-02 15:04:05"))
-}
-
 func main() {
-	now := time.Now()
-	t2()
-	fmt.Println("查询耗时：", time.Since(now))
+	m := make(map[string]string)
+	fmt.Println(len(m))
+	var n map[string]string = nil
+	fmt.Println(len(n))
 
-	err2 := os.MkdirAll("D:\\logs\\1\\2", 0755)
-	fmt.Println(err2)
-	//rdb := redis.NewClient(&redis.Options{
-	//	Addr:     "localhost:6379",
-	//	Password: "", // no password set
-	//	DB:       0,  // use default DB
-	//})
-	//m := make(map[string]interface{})
-	//m["vsclamav"] = "1234"
-	//m["vscomod"] = "456"
-	//rdb.HSet(context.Background(), "kafkaMetricInfo", m)
+	d := filepath.Join("/data", "sadeq", "fdsg")
+	all := strings.ReplaceAll(d, "\\", "/")
+	fmt.Println(all)
+	fmt.Println(time.Now().Unix())
 
-	//dir := filepath.Dir("/data/file/1.txt")
-	//base := filepath.Base("/data/file/1.txt")
-	//
-	//fmt.Println(dir)
-	//fmt.Println(base)
-	//fmt.Println(filepath.Join(dir, base))
-	//lstat, err2 := os.Stat("D:\\logs\\eng\\1234\\file")
-	//fmt.Println(err2)
-	//fmt.Println(lstat)
-}
-
-func GetSHA256(b []byte) string {
-	h := sha256.New()
-	h.Write(b)
-	return hex.EncodeToString(h.Sum(nil))
-}
-
-func t1() {
-	//配置
-	config = clientv3.Config{
-		Endpoints:   []string{"192.168.102.155:2379"},
-		Username:    "app",
-		Password:    "psKdqSe",
-		DialTimeout: time.Second * 5,
-	}
-	//连接 床见一个客户端
-	if client, err = clientv3.New(config); err != nil {
-		fmt.Println("clientv3.New : ", err)
-		return
-	}
-	//用于读写etcd的键值对
-	kv = clientv3.NewKV(client)
-	put(kv)
-	get(kv)
-}
-
-func get(kv clientv3.KV) {
-	resp, err := kv.Get(context.TODO(), path)
-	if err != nil {
-		fmt.Println(" kv.Get : ", err)
-		return
-	}
-	for i := range resp.Kvs {
-		if v := resp.Kvs[i].Value; v != nil {
-			fmt.Println(string(v))
-		}
-	}
-	if resp.Count == 0 {
-		fmt.Println("没有获取到数据")
-		return
-	}
-	fmt.Println(string(resp.Kvs[0].Value))
-}
-
-func put(kv clientv3.KV) {
-	en := make(map[string]bool)
-	en["en1"] = true
-	en["en2"] = true
-	en["en3"] = true
-	en["en4"] = true
-	en["en5"] = true
-	en["en6"] = true
-	marshal, _ := json.Marshal(en)
-	putResp, err = kv.Put(context.TODO(), path, string(marshal), clientv3.WithPrevKV())
-	if err != nil {
-		fmt.Println(" kv.Put : ", err)
-	} else {
-		//获取版本信息
-		fmt.Println("Revision:", putResp.Header.Revision)
-		if putResp.PrevKv != nil {
-			fmt.Println("key:", string(putResp.PrevKv.Key))
-			fmt.Println("Value:", string(putResp.PrevKv.Value))
-			fmt.Println("Version:", string(putResp.PrevKv.Version))
-		}
-	}
+	flag.Int64Var(&max, "max", 10, "set max count , default 10")
+	flag.Parse()
+	//for {
+	//	time.Sleep(time.Second * 3)
+	//	cmd := exec.CommandContext(ctx, "tail", "-1", "/var/log/messages")
+	//	output, _ := cmd.Output()
+	//	r := string(output)
+	//	log.Println(r)
+	//	//r := "Mar 28 10:54:58 10-52-6-85 csinterface: [GIN] 2022/03/28 - 10:54:58 | 200 | 11.288055ms | 10.10.14.79 | POST \"/v1/file/sha256/batch\""
+	//	if strings.Contains(strings.ToLower(r), "[gin]") &&
+	//		strings.Contains(strings.ToLower(r), "csinterface") &&
+	//		strings.Contains(strings.ToLower(r), "/v1/file/sha256/") {
+	//		split := strings.Split(strings.ToLower(r), "[gin]")
+	//		s := split[1]
+	//		rs := strings.Split(s, "|")
+	//		//fmt.Println(strings.TrimSpace(rs[0]))
+	//		timeTemplate := "2006/01/02 - 15:04:05"
+	//		stamp, _ := time.ParseInLocation(timeTemplate, strings.TrimSpace(rs[0]), time.Local)
+	//		subSec := time.Now().Sub(stamp).Seconds()
+	//		if subSec < 3 {
+	//			count++
+	//			log.Println("累计次数 ", count)
+	//		}
+	//		if count >= max {
+	//			//开始报警
+	//			log.Println("wx 报警开始")
+	//			wxNotify("云查接口正在被调用", "")
+	//			notice++
+	//		}
+	//		if notice > 3 {
+	//			log.Println("wx 报警阈值达到 3次")
+	//			time.Sleep(time.Minute * 30)
+	//			notice = 1
+	//		}
+	//	} else {
+	//		log.Println("not gin log")
+	//	}
+	//}
 }
